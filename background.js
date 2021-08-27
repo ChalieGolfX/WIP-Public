1d5d2fdce4dd8fce5f6905577595e5faf4fbedd4
ChalieGolfX <89253556+ChalieGolfX@users.noreply.github.com>
2021-08-27 23:43:48 +0200

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-// This is the background JS file
-
-async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-  }
