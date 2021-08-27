64a0de49b055fbeae23f8f87debff37e69f4ee89
ChalieGolfX <89253556+ChalieGolfX@users.noreply.github.com>
2021-08-27 23:44:04 +0200

    Delete language.js

--- language.js
+++
@@ -1,5 +0,0 @@
-$('#Change').click(function() {
-  var url = window.location.href.replace('en-US', 'nb-NO');
-  document.location.href = url;
-  alert('Button works'); //Alert to verify button click works
-})
